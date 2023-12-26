69d9ff5fcc7a4b9c39a876ea240308810440344a
pawank53 <pawankumar930456@gmail.com>
2023-12-27 02:48:25 +0530

    This is a background changer application

---
+++ App.tsx
@@ -0,0 +1,57 @@
+
+import React, { useState } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+    const [randomBackground, setRandomBackground]=useState("#ffffff");
+
+    const generateColor=()=>{
+      const hexRange='0123456789ABCDEF';
+      let color="#";
+      for(let i=0;i<6;i++){
+        color +=hexRange[Math.floor(Math.random() * 16)];
+      }
+      setRandomBackground(color);
+    }
+
+  return (
+    <>
+    <StatusBar backgroundColor={randomBackground}/>
+    <View style={[styles.container, {backgroundColor:randomBackground}]}>
+      <TouchableOpacity onPress={generateColor}>
+        <View style={styles.actionBtn}>
+          <Text style={styles.actionBtnText}>Press me </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  actionBtn:{
+    borderRadius:12,
+    backgroundColor:'#6A1B41',
+    paddingVertical:10,
+    paddingHorizontal:40
+  },
+  actionBtnText:{
+    color:"#ffffff",
+    fontSize:20,
+    textTransform:'uppercase'
+  }
+});
+
+export default App;
